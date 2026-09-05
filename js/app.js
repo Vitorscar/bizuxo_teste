86572b75406c3f2ba385682079f3fea722f99ced
Vitorscar <vitorsander2008@gmail.com>
2026-09-05 19:19:07 -0300

    Add Service Worker registration in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-
+// Registro do Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker registrado com sucesso:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Falha ao registrar o Service Worker:', error);
+      });
+  });
+}
